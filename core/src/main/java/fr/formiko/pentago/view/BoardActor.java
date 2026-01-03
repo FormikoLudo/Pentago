c40be3427a02cc5f88ccce66f3b5d783c24b76ff
HydrolienF <hydrolien.f@gmail.com>
2026-01-03 18:33:18 +0100

    Better board background with darker line

--- core/src/main/java/fr/formiko/pentago/view/BoardActor.java
+++ core/src/main/java/fr/formiko/pentago/view/BoardActor.java
@@ -3,6 +3,7 @@ package fr.formiko.pentago.view;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.utils.Align;
 import fr.formiko.pentago.Main;
 import fr.formiko.pentago.model.Board;
 import fr.formiko.pentago.model.Point;
@@ -12,6 +13,8 @@ import space.earlygrey.shapedrawer.ShapeDrawer;
 
 public class BoardActor extends Group {
     private static final Color backgroundColor = Color.BROWN;
+    private static final Color lineColor = new Color(0x552a03ff);
+    private static final int lineSize = 4;
     private Board board;
     private Map<Point, CellActor> cells;
 
@@ -22,7 +25,6 @@ public class BoardActor extends Group {
         int heightCell = height / Board.SIZE;
         for (int i = 0; i < Board.SIZE; i++) {
             for (int j = 0; j < Board.SIZE; j++) {
-                // sb.append(getState(new Point(i, j)));
                 CellActor cellActor = new CellActor();
                 cellActor.setPosition(i * widthCell, j * heightCell);
                 cellActor.setSize(widthCell, heightCell);
@@ -42,6 +44,8 @@ public class BoardActor extends Group {
     public void draw(Batch batch, float parentAlpha) {
         ShapeDrawer shape = Main.getShapeDrawer(batch);
         shape.filledRectangle(this.getX(), this.getY(), this.getWidth(), this.getHeight(), backgroundColor);
+        shape.filledRectangle(0, this.getY(Align.center) - lineSize, this.getWidth(), lineSize, lineColor);
+        shape.filledRectangle(this.getX(Align.center) - lineSize, 0, lineSize, this.getHeight(), lineColor);
         super.draw(batch, parentAlpha);
     }
 }
